a6d63020c5ec6cb62bbb68ad1d3b2e831e94be91
Kostas Bariotis <konmpar@gmail.com>
2016-11-02 17:56:32 +0200

    webpack added, restrure folders

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,36 @@
+import '../sass/boot.scss';
+
+import $ from 'jquery';
+import Pjax from 'pjax';
+import hljs from 'highlight.js';
+
+window.jQuery = $;
+window.$ = $;
+window.Pjax = Pjax;
+window.hljs = hljs;
+
 $(document).ready(function () {
+
   new Pjax({
-    elements: "a", // default is "a[href], form[action]"
-    selectors: ["title", "body"],
+    elements: 'a', // default is 'a[href], form[action]'
+    selectors: ['title', 'body'],
     scrollTo: 0,
     analytics: function () {
+
       if (window.ga) {
         var d = document.location.pathname + document.location.search + document.location.hash;
-        ga("send", "pageview", d);
+        ga('send', 'pageview', d);
       }
     }
   });
 
   $(document).on('pjax:complete', function () {
-    $('pre code').each(function(i, block) {
+
+    $('pre code').each(function (i, block) {
+
       hljs.highlightBlock(block);
     });
   });
 
   hljs.initHighlightingOnLoad();
-
 });

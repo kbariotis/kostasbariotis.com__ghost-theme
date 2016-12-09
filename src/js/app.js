5b9e8c0e7ec2bfee3685f2356bdf77b710dddf56
Kostas Bariotis <konmpar@gmail.com>
2016-12-09 14:53:59 +0200

    fix analytics

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,32 @@
 import '../sass/boot.scss';
+import 'bootstrap-sass/assets/javascripts/bootstrap/collapse.js';
 
-import $ from 'jquery';
 import Pjax from 'pjax';
 import hljs from 'highlight.js';
 
-window.jQuery = $;
-window.$ = $;
 window.Pjax = Pjax;
 window.hljs = hljs;
 
-$(document).ready(function () {
+new Pjax({
+  elements: 'a', // default is 'a[href], form[action]'
+  selectors: ['title', 'body'],
+  scrollTo: 0,
+  analytics: function () {
 
-  new Pjax({
-    elements: 'a', // default is 'a[href], form[action]'
-    selectors: ['title', 'body'],
-    scrollTo: 0,
-    analytics: function () {
-
-      if (window.ga) {
-        var d = document.location.pathname + document.location.search + document.location.hash;
-        ga('send', 'pageview', d);
-      }
+    if (window.ga) {
+      var d = document.location.pathname + document.location.search + document.location.hash;
+      ga('send', 'pageview', d);
     }
-  });
-
-  $(document).on('pjax:complete', function () {
-
-    $('pre code').each(function (i, block) {
-
-      hljs.highlightBlock(block);
-    });
-  });
-
-  hljs.initHighlightingOnLoad();
+  }
 });
+
+document.addEventListener('pjax:complete', function () {
+
+  var blocks = document.querySelectorAll('pre code');
+
+  blocks.forEach(function (block) {
+    hljs.highlightBlock(block);
+  });
+});
+
+hljs.initHighlightingOnLoad();
